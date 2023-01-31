eacf7f70facb33de1e6832bbaeabbf06e0a6f056
WhiteHole <101702658+WhiteHole00@users.noreply.github.com>
2023-02-01 04:21:20 +0900

    Update Group.py

--- Group.py
+++ Group.py
@@ -15,6 +15,7 @@ if __name__ == "__main__":
         input("모듈 설치를 완료 하였습니다.\ntoken.txt 를 만들고 안에 토큰을 넣어 주세요.")
     else:
         count = int(input("만드실 횟수를 입력 해 주세요 >> "))
+        add = int(input("추가 하실 유저 아이디를 입력 해 주세요 >> "))
         token = Path("token.txt").read_text()
         if "." in token:
             def header():
@@ -31,25 +32,32 @@ if __name__ == "__main__":
             name = random.choice(random_name.read().split("\n"))
             print(name)
             try:
-                for _ in range(1,count+1):
-                    sleep(0.4)
-                    #print(token)
+                for _ in range(1, count+1):
+                    sleep(0.2)
                     payload = {
                         "recipients": []
                     }
                     r = requests.post('https://discord.com/api/v10/users/@me/channels', headers=header(), json=payload)
-                    data = json.loads(r.content)
+                    data = r.json()
                     bot = discum.Client(token = token,log=False)
                     bot.setDmGroupIcon(data['id'], "./img/None.png")
                     js = {
                         'name': name
                     }
-                    res = requests.patch(f'https://discord.com/api/v10/channels/{data["id"]}', headers=header(),json=js)
+                    res = requests.patch(f'https://discord.com/api/v9/channels/{data["id"]}', headers=header(),json=js)
 
                     if res.status_code == 200 or res.status_code == 204 or res.status_code == 201:
                         print("SUCCESS | {}".format(name))
+                        resp = requests.put(f'https://discord.com/api/v9/channels/{data["id"]}/recipients/{add}', headers=header())
+                        if resp.status_code == 200 or resp.status_code == 204 or resp.status_code == 201:
+                            print(f"{data['id']} | {add} 추가 성공!")
+                        elif resp.status_code == 429:
+                            input(f"{resp.json()['retry_after']} 후에 다시 시도 해 주세요.")
+                        else:
+                            input("알 수 없는 오류가 발생하였습니다.")
                     else:
                         print("FAIL | {}".format(name))
+                input("모든 작업이 완료 되었습니다.")
 
             except Exception as e:
                 print(e)
